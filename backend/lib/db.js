f8a0e65f9b9a59a1d8369cc91a85e73e46f6fca1
shiva <tsivavenkatkumar@gmail.com>
2025-03-10 19:23:51 +0530

    deploy

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
-
+import { configDotenv } from "dotenv";
+configDotenv();
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/clone");
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
-
-// mongodb+srv://sanjayketham2004:eHUeqz4cpunDlOvB@clone.aamrs.mongodb.net/?retryWrites=true&w=majority&appName=Clone
--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
-}
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
